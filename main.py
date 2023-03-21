8eff3bbc80f111d7968f6c52be2ee3bd2db93e1d
Ema Baranauskaite <emabaranauskaite@Emas-MacBook-Pro.local>
2023-03-21 18:29:12 +0200

    fix on urllib

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import joblib
+import urllib.request
+import pickle
 import pandas as pd
-from urllib.request import urlopen 
 from fastapi import FastAPI
 
 app = FastAPI()
-model = joblib.load(urlopen("https://storage.cloud.google.com/modelstep1/step1-status_classifier.joblib"))
-  
+
+model = pickle.load(urllib.request.urlopen("https://storage.cloud.google.com/modelstep1/step1-status_classifier.joblib"))
+ 
 @app.get("/")
 def home():
     return {"message":"Hello with model"}
