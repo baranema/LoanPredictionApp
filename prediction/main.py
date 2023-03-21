282c183890f8bdf6d820be1131f7756c5cffed87
Ema Baranauskaite <emabaranauskaite@Emas-MacBook-Pro.local>
2023-03-21 22:24:39 +0200

    fixed

--- prediction/main.py
+++ prediction/main.py
@@ -1,9 +1,9 @@
-#import joblib
+import joblib
 from fastapi import FastAPI
 
 app = FastAPI()
-#model = joblib.load("prediction/models/step1-status_classifier.joblib")
+model = joblib.load("prediction/models/step1-status_classifier.joblib")
 
 @app.get("/")
 def home():
-    return {"message":"Hello with asdfasdf"}
+    return {"message":"Hello with model"}
