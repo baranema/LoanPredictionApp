396188d380a1f6d30f8276c431156ee9b23badf5
Ema Baranauskaite <emabaranauskaite@Emas-MacBook-Pro.local>
2023-03-21 22:33:48 +0200

    redo:

--- prediction/main.py
+++ prediction/main.py
@@ -1,13 +1,10 @@
 import joblib
 from fastapi import FastAPI
-import joblib
-import pandas as pd
-from fastapi import FastAPI
-from step1_loan import LoanStep1
+from prediction.step1_loan import LoanStep1
 
 app = FastAPI()
 model = joblib.load("prediction/models/step1-status_classifier.joblib")
 
 @app.get("/")
 def home():
-    return {"message":"Hello with model"}
+    return {"message":"Hello with model!!!"}
