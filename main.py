629dd76085afa062c49598bb81eaac7aa70ef10b
Ema Baranauskaite <emabaranauskaite@Emas-MacBook-Pro.local>
2023-03-21 18:24:53 +0200

    fix

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import joblib
 import pandas as pd
+from urllib.request import urlopen
+from sklearn.externals import joblib
 from fastapi import FastAPI
 
 app = FastAPI()
-model = joblib.load("https://storage.cloud.google.com/modelstep1/step1-status_classifier.joblib")
- 
+model = joblib.load(urlopen("https://storage.cloud.google.com/modelstep1/step1-status_classifier.joblib"))
+  
 @app.get("/")
 def home():
     return {"message":"Hello with model"}
