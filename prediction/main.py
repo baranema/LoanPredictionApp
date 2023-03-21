bba397b2b2508db70b2de3632192505462601bf8
Ema Baranauskaite <emabaranauskaite@Emas-MacBook-Pro.local>
2023-03-21 21:55:29 +0200

    removed

--- prediction/main.py
+++ prediction/main.py
@@ -3,9 +3,9 @@ from io import BytesIO
 import pickle
 import requests
 
-mLink = "https://github.com/baranema/test_fastapi/blob/main/prediction/models/step1-status_classifier.pkl?raw=true"
-mfile = BytesIO(requests.get(mLink).content)
-model = pickle.load(mfile)
+# mLink = "https://github.com/baranema/test_fastapi/blob/main/prediction/models/step1-status_classifier.pkl?raw=true"
+# mfile = BytesIO(requests.get(mLink).content)
+# model = pickle.load(mfile)
 
 app = FastAPI() 
 
