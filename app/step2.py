affec9e3677ee878466564f1c61ad7b9b85f9a16
Ema Baranauskaite <emabaranauskaite@Emas-MacBook-Pro.local>
2023-03-23 16:03:39 +0200

    cat grade

--- app/step2.py
+++ app/step2.py
@@ -25,17 +25,19 @@ def grade_pred():
     if st.button('Predict'):
         status, predictions = get_prediction(loan_info)
         df = pd.DataFrame(loan_info)
+        df.insert(0,'grade_category','')
+        df['grade_category'] = "Unknown" 
         df.insert(0,'grade','')
-        df['grade'] = "Unknown" 
+        df['grade'] = "Unknown"
 
         if status == 200: 
             for index, prediction in predictions.items(): 
                 new_index = int(index)
  
                 if prediction is not None:
-                    df.at[new_index, 'grade']= prediction
+                    df.at[new_index, 'grade_category']= prediction
             
-            df = df.sort_values(by="grade", ascending=True) 
+            df = df.sort_values(by="grade_category", ascending=True) 
             st.dataframe(df) 
         else:
             st.write(f'Sorry, there was an error making the prediction. Please try again later. Error message - {predictions}')
