986bce153ef853200120d1e4c6faddb98ef39b71
Ema Baranauskaite <emabaranauskaite@Emas-MacBook-Pro.local>
2023-03-25 22:29:16 +0200

    step4

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from step1 import acc_pred
 from step2 import grade_pred
 from step3 import subgrade_pred
+from step4 import int_rate_pred
 
 def main_page():
     st.markdown("# Lending Club Loans App 🎈")
@@ -19,11 +20,16 @@ def step3():
     subgrade_pred()
     st.sidebar.markdown("### Loan Subgrade Prediction")
 
+def step4():
+    int_rate_pred()
+    st.sidebar.markdown("### Loan Interest Rate Prediction")
+
 page_names_to_funcs = {
     "Home": main_page,
     "Loan Acceptance": step1,
     "Loan Grade": step2,
-    "Loan Subgrade": step3
+    "Loan Subgrade": step3,
+    "Loan Interest Rate": step4
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
---
+++ app/step4.py
@@ -0,0 +1,42 @@
+import requests
+import streamlit as st 
+import pandas as pd 
+ 
+API_URL = 'https://eb-loan-prediction-backend.herokuapp.com/step4_int_rate_prediction/'
+
+def int_rate_pred():
+    st.title('Loan Subrade Prediction')
+    st.write('Upload your csv file to check anticipatory subgrade of your loan:')
+ 
+    st.selectbox('Input Type', ['CSV Upload'])
+    
+    uploaded_file = st.file_uploader('Upload CSV file')
+    
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        loan_info = df.to_dict(orient='records')
+    else:
+        loan_info = []
+
+    def get_prediction(payload): 
+        response = requests.post(API_URL, json=payload) 
+        return response.status_code, response.json()
+ 
+    if st.button('Predict'):
+        if uploaded_file is not None:
+            status, predictions = get_prediction(loan_info)
+            df = pd.DataFrame(loan_info)
+            df.insert(0,'int_rate','')
+            df['int_rate'] = "Unknown"
+            
+            if status == 200: 
+                for index, prediction in predictions.items():
+                    new_index = int(index)
+                    # if prediction is not None:
+                    #     df.at[new_index, 'int_rate']= prediction
+                             
+               # df = df.sort_values(by="int_rate", ascending=False) 
+                st.dataframe(df) 
+                
+            else:
+                st.write(f'Sorry, there was an error making the prediction. Please try again later. Error message - {predictions}')
